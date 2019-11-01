dc0650e47f61f2394d3528734b8f21058fcf6f05
Hung <trunghung0962910926@gmail.com>
2019-11-01 08:13:09 +0700

    KTPMDatBan

---
+++ Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace DatBan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.txtSodienthoai = new System.Windows.Forms.TextBox();
+            this.cbSokhach = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbThoigian = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbDiachi = new System.Windows.Forms.ComboBox();
+            this.btnXacnhan = new System.Windows.Forms.Button();
+            this.btnNhaplai = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(114, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thông Tin Đặt Bàn";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Họ Tên";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Số điện thoại";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ngày tháng đặt chỗ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(121, 34);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(200, 20);
+            this.txtHoTen.TabIndex = 5;
+            this.txtHoTen.TextChanged += new System.EventHandler(this.txtHoTen_TextChanged);
+            // 
+            // txtSodienthoai
+            // 
+            this.txtSodienthoai.Location = new System.Drawing.Point(121, 93);
+            this.txtSodienthoai.Name = "txtSodienthoai";
+            this.txtSodienthoai.Size = new System.Drawing.Size(200, 20);
+            this.txtSodienthoai.TabIndex = 7;
+            // 
+            // cbSokhach
+            // 
+            this.cbSokhach.FormattingEnabled = true;
+            this.cbSokhach.Location = new System.Drawing.Point(121, 66);
+            this.cbSokhach.Name = "cbSokhach";
+            this.cbSokhach.Size = new System.Drawing.Size(200, 21);
+            this.cbSokhach.TabIndex = 8;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "";
+            this.dateTimePicker1.Location = new System.Drawing.Point(121, 119);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Số khách";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 150);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Thời gian đến";
+            // 
+            // cbThoigian
+            // 
+            this.cbThoigian.FormattingEnabled = true;
+            this.cbThoigian.Location = new System.Drawing.Point(121, 146);
+            this.cbThoigian.Name = "cbThoigian";
+            this.cbThoigian.Size = new System.Drawing.Size(200, 21);
+            this.cbThoigian.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 176);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Địa chỉ nhà hàng";
+            // 
+            // cbDiachi
+            // 
+            this.cbDiachi.FormattingEnabled = true;
+            this.cbDiachi.Location = new System.Drawing.Point(121, 173);
+            this.cbDiachi.Name = "cbDiachi";
+            this.cbDiachi.Size = new System.Drawing.Size(200, 21);
+            this.cbDiachi.TabIndex = 14;
+            // 
+            // btnXacnhan
+            // 
+            this.btnXacnhan.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnXacnhan.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnXacnhan.Location = new System.Drawing.Point(39, 287);
+            this.btnXacnhan.Name = "btnXacnhan";
+            this.btnXacnhan.Size = new System.Drawing.Size(75, 23);
+            this.btnXacnhan.TabIndex = 15;
+            this.btnXacnhan.Text = "Xác nhận";
+            this.btnXacnhan.UseVisualStyleBackColor = false;
+            this.btnXacnhan.Click += new System.EventHandler(this.btnXacnhan_Click);
+            // 
+            // btnNhaplai
+            // 
+            this.btnNhaplai.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnNhaplai.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnNhaplai.Location = new System.Drawing.Point(120, 287);
+            this.btnNhaplai.Name = "btnNhaplai";
+            this.btnNhaplai.Size = new System.Drawing.Size(75, 23);
+            this.btnNhaplai.TabIndex = 16;
+            this.btnNhaplai.Text = "Nhập lại";
+            this.btnNhaplai.UseVisualStyleBackColor = false;
+            this.btnNhaplai.Click += new System.EventHandler(this.btnNhaplai_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnThoat.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnThoat.Location = new System.Drawing.Point(201, 287);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 17;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = false;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtSodienthoai);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cbDiachi);
+            this.groupBox1.Controls.Add(this.txtHoTen);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.cbSokhach);
+            this.groupBox1.Controls.Add(this.cbThoigian);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(39, 60);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(354, 221);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông Tin";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(433, 324);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnNhaplai);
+            this.Controls.Add(this.btnXacnhan);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Nhà Hàng Bé Na";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.TextBox txtSodienthoai;
+        private System.Windows.Forms.ComboBox cbSokhach;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbThoigian;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cbDiachi;
+        private System.Windows.Forms.Button btnXacnhan;
+        private System.Windows.Forms.Button btnNhaplai;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatBan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+            cbSokhach.Items.Add("5");
+            cbSokhach.Items.Add("10");
+            cbSokhach.Items.Add("15");
+            cbThoigian.Items.Add("6h");
+            cbThoigian.Items.Add("12h");
+            cbThoigian.Items.Add("7h");
+            cbDiachi.Items.Add("736 Nguyễn Trãi,P.2,Q.5");
+            cbDiachi.Items.Add("75 3 tháng 2,P.3,Q.11");
+            cbDiachi.Items.Add("123 Nguyễn Chí Thanh,P.11,Q.11");
+
+
+        }
+       
+        public string HoTen,ThoiGian,Diachi;
+        public int SoKhach, DienThoai;
+        public DateTime NgayThang;
+        public void laydulieu()
+        {
+            HoTen = txtHoTen.Text;
+            SoKhach = Int32.Parse(cbSokhach.SelectedItem.ToString());
+            DienThoai = Int32.Parse(txtSodienthoai.Text.ToString());
+            NgayThang = dateTimePicker1.Value.Date;
+            ThoiGian = cbThoigian.Text;
+            Diachi = cbDiachi.Text;
+        }
+        public void nhapdulieu()
+        {
+            //getMonAn();
+            Hotro.ketnoi();
+            Hotro.ThucThiLenh("INSERT INTO DAT_BAN VALUES('" + HoTen + "', " + SoKhach + ", " + DienThoai + ", '" + NgayThang + "','"+ ThoiGian + "','"+ Diachi + "')");
+            MessageBox.Show("Thêm dữ liệu thành công!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtHoTen_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnXacnhan_Click(object sender, EventArgs e)
+        {
+            
+            laydulieu();
+            nhapdulieu();
+        }
+
+        private void btnNhaplai_Click(object sender, EventArgs e)
+        {
+            txtHoTen.Text= " ";
+            txtSodienthoai.Text = "";
+            cbSokhach.Text = "";
+            cbThoigian.Text = "";
+            cbDiachi.Text = "";
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
